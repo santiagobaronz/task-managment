e03a357392facba6c79ab8fb722acbd3dbb71083
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2022-05-24 20:03:28 -0500

    [Primera subida] Diseño UI y funciones básicas

---
+++ js/script.js
@@ -0,0 +1,182 @@
+'use strict';
+
+// Date
+
+const dateTitle = document.querySelector("#date");
+const date = new Date();
+
+const days = {
+    0: "Domingo", 1: "Lunes", 2: "Martes", 3: "Miercoles",
+    4: "Jueves", 5: "Viernes", 6: "Sábado"
+}
+
+const abbreviatedDays = {
+    0: "Dom", 1: "Lun", 2: "Mar", 3: "Mier",
+    4: "Jue", 5: "Vie", 6: "Sáb"
+}
+
+const months = {
+    0: "Enero", 1: "Febrero", 2: "Marzo", 3: "Abril",
+    4: "Mayo", 5: "Junio", 6: "Julio", 7: "Agosto",
+    8: "Septiembre", 9: "Octubre", 10: "Noviembre", 11: "Diciembre"
+}
+
+const maxDaysPerMonth = {
+    0: "Enero", 1: "Febrero", 2: "Marzo", 3: "Abril",
+    4: "Mayo", 5: "Junio", 6: "Julio", 7: "Agosto",
+    8: "Septiembre", 9: "Octubre", 10: "Noviembre", 11: "Diciembre"
+}
+
+dateTitle.innerHTML =   months[date.getMonth()]+ ", " + 
+                        days[date.getDay()] + " " + 
+                        date.getDate();
+
+// Calendar
+
+function GetDaysLeft(year, month){
+    const daysLeft = new Date(year, month, 0);
+    return daysLeft.getDate();
+}
+
+const currentDay = date.getDate();
+const currentYear = date.getFullYear();
+const month = date.getMonth() + 1;
+const daysLeft = GetDaysLeft(currentYear,month);
+
+const calendar = document.getElementById("calendar")
+
+var daysArray = []
+ 
+for (let i = currentDay; i <= daysLeft; i++) {
+
+    let div = document.createElement("div");
+    i == currentDay ? div.className = "day-box date-selected date-selected-color" : div.className = "day-box no-selected";
+    div.id = i;
+
+    let dayCalendar = new Date(currentYear, month - 1, i);
+
+
+    div.innerHTML = `<p class='day-number'>${i}</p>
+                    <p class='day-name'>${abbreviatedDays[dayCalendar.getDay()]}</p>`;
+
+    calendar.appendChild(div)
+    
+    var elementCalendar = document.getElementById(i);
+    daysArray.push(elementCalendar);
+}
+
+// Change color of day selected
+
+const changeDayInformation = function () {
+	let daySelected = this;
+
+    for (let i = 0; i < daysArray.length; i++) {
+        if(i != daySelected){
+            daysArray[i].classList.remove("date-selected-color")
+            daysArray[i].classList.remove("date-selected")
+            daysArray[i].classList.add("no-selected");
+        }
+    }
+    
+    daySelected.classList.add("date-selected-color")
+    daySelected.classList.add("date-selected")
+    daySelected.classList.remove("no-selected")
+
+}
+
+daysArray.forEach(days => { days.addEventListener("click", changeDayInformation)});
+
+// Creating a new task
+
+const developDiv = document.getElementById("develop-div");
+const universityDiv = document.getElementById("university-div");
+const homeDiv = document.getElementById("home-div");
+
+const form = document.getElementById("form")
+
+developDiv.addEventListener("click", () => {
+    universityDiv.classList.remove("university-selected");
+    homeDiv.classList.remove("home-selected");
+    developDiv.classList.add("develop-selected");
+},false);
+
+universityDiv.addEventListener("click", () => {
+    developDiv.classList.remove("develop-selected");
+    homeDiv.classList.remove("home-selected");
+    universityDiv.classList.add("university-selected");
+},false);
+
+homeDiv.addEventListener("click", () => {
+    developDiv.classList.remove("develop-selected");
+    universityDiv.classList.remove("university-selected");
+    homeDiv.classList.add("home-selected");
+},false);
+
+
+var taskArray = [];
+
+function SaveTask(task){
+    taskArray.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var data = new FormData(form);
+
+    var taskName = data.get("task-name");
+    var taskDate = data.get("datepicker");
+    var taskStartTime = data.get("task-start-time");
+    var taskFinishTime = data.get("task-finish-time");
+    var taskInfo = data.get("task-info");
+    var taskCategory = data.get("category");
+
+    if(taskName == "" || taskDate == "" || taskStartTime == ""
+    || taskFinishTime == "" || taskCategory == ""){
+
+        const error = document.getElementById("error");
+        error.classList.add("error");
+        error.innerHTML = "Hay campos que aún debes rellenar";
+
+    }else{
+
+        class Task {
+            constructor(name, date, stime, ftime, category, comment = "") {
+                this.comment = comment;
+                this.category = category;
+                this.startTime = stime;
+                this.finishTime = ftime;
+                this.date = date;
+                this.name = name;
+            }
+        }
+
+        var newTask = new Task(taskName,taskDate,taskStartTime,taskFinishTime,taskInfo,taskCategory);
+
+        SaveTask(newTask);
+
+    }
+
+    console.log(taskArray)
+
+},false)
+
+
+
+$(function () {
+    $("#datepicker").datepicker();
+    });
+
+    $('.timepicker').timepicker({
+        timeFormat: 'h:mm p',
+        interval: 30,
+        minTime: '00:00am',
+        maxTime: '11:30pm',
+        defaultTime: '13',
+        startTime: '0',
+        dynamic: true,
+        dropdown: true,
+        scrollbar: true
+    });
